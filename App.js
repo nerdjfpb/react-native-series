1cb149302f80da87bcbeeef73fd6622738e59288
Muhammad Ali <nerdjfpb@gmail.com>
2020-03-28 20:05:40 +0600

    Day 3

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text style={styles.mainText}>
-				Welcome to Nerdjfpb React Native Series
-			</Text>
-		</View>
-	)
+export default class App extends React.Component {
+	state = {
+		counter: 0
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.mainText}>Counter: {this.state.counter}</Text>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
